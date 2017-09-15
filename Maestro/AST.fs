d1a9396b3e130b7c2a5c6c24f4a5772784ea3ac1
Dakota Murphy <dakotamurphyucf@knights.ucf.edu>
2017-09-14 22:26:11 -0400

    update types

--- Maestro/AST.fs
+++ Maestro/AST.fs
@@ -5,12 +5,41 @@ open Maestro.Types
 module AST = 
     
     type Dependency = {name: string; program_id: string;}
-    
+     
     type Input = {_type: Mtype; name: string;} 
+    
+    type Referral = {
+        program_id: string
+        program_name: string
+        return_type: Mtype
+        index: int
+        input: list<Input>
+        nextblock: int
+    }
+    
+    type Lamda = {
+        return_type: Mtype
+        index: int
+        input: list<Input>
+        statement: string
+        nextblock: int 
+    } 
     
     type ProgramBlock = Referral | Lamda
     
-    type ComputationType = Pure | IO 
+    type IOFile = Main | Child 
+    
+    type ComputationType = Pure | IO of IOFile
+    
+    type Error = Type | Syntax | Other of string
+    
+    type ProgramError = {_type: Error; description: string; index:int}
+    
+    type Validation = | Valid | InValid of ProgramError
+    
+    type Author = | Unknown | Person of string
+    
+    type Access = Public | Private
     
     type Program = {
         id: string
@@ -21,6 +50,14 @@ module AST =
         dependencies: list<Dependency>
         inputs: list<Input>
         pipeline: list<ProgramBlock>
+        description: string
+        html_description: string
+        valid: Validation
+        version: string
+        author: Author
+        access: Access
+        subProgram: list<int>
+        globals: list<string>
     }
     
     
